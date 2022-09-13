4c920843025bb1f6a557d78888180e11a243743f
Marcelo De Lima <delima_marcelo@outlook.com>
2022-09-13 08:27:39 -0300

    fix: add database ao connect

--- src/models/db/connection.js
+++ src/models/db/connection.js
@@ -6,6 +6,7 @@ const connection = mysql.createPool({
   host: process.env.MYQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
 module.exports = connection;
