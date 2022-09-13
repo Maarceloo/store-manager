8446500c98734950c5981f9937f604f4a274bc7e
Marcelo De Lima <delima_marcelo@outlook.com>
2022-09-12 23:52:36 -0300

    test: ProductsServices Coberto

---
+++ tests/unit/services/productsServices.test.js
@@ -0,0 +1,39 @@
+const sinon = require("sinon")
+const chai = require("chai")
+const { expect } = chai;
+const sinonChai = require("sinon-chai");
+chai.use(sinonChai)
+
+const productsModel = require('../../../src/models/productsModels');
+const productService = require('../../../src/services/productService');
+
+const { products, productsDB , newProducts} = require('../models/mocks/productsAllMock');
+
+
+describe('Testes na unidade de productsServices', () => {
+
+  it('Realizando um GET em todos os produtos', async () => {
+    sinon.stub(productsModel, 'getAllProductsModels').resolves(productsDB)
+    const result = await productService.getAllProducts();
+    expect(result).to.be.deep.equal(products);
+  })
+  
+  it('Realizando um GET pelo ID de um produto EXISTENTE', async () => {
+
+      sinon.stub(productsModel, 'getIdProductsModels').resolves([productsDB[0]]);
+      const result = await productService.getIdProducts(1);
+      expect(result).to.be.deep.equal(products[0]);
+      sinon.restore()
+    })
+  
+  it('Cadastra um novo produto no DB', async () => {
+      const req = {body: {name: 'Mouse'}}
+
+      sinon.stub(productsModel, 'postNewProductModels').resolves(5)
+      sinon.stub(productsModel, 'getIdProductsModels').resolves([newProducts])
+      const result = await productService.newProducts(req);
+
+      expect(result).to.be.deep.equal(newProducts);
+      sinon.restore()
+  })
+})
