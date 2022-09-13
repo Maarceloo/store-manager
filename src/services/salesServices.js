062c62a7a98239135770e0f65eec33072c16d5f0
Marcelo De Lima <delima_marcelo@outlook.com>
2022-09-13 10:30:30 -0300

    desenvolvendo salesService

---
+++ src/services/salesServices.js
@@ -0,0 +1,8 @@
+/* const newSalesServices = async (req) => {
+  const { name } = req.body;
+  const resultId = await productsModel.postNewProductModels(name);
+  const [newObj] = await productsModel.getIdProductsModels(resultId);
+  return newObj;
+};
+
+module.exports = { newSalesServices }; */
