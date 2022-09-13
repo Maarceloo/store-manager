93a1fecaf8789ecdc88c9c595dcd34726b66ad05
Marcelo De Lima <delima_marcelo@outlook.com>
2022-09-13 15:41:47 -0300

    feat: salesModels implementado

--- src/models/salesModels.js
+++ src/models/salesModels.js
@@ -14,4 +14,34 @@ const addSalesDBModels = async (id, productId, quantity) => {
   );
 };
 
-module.exports = { newSalesDateModels, addSalesDBModels };
+const getAllSalesModels = async () => {
+  const [response] = await connection.execute(
+    `SELECT P.sale_id AS saleId, S.date, P.product_id AS productId, P.quantity 
+      FROM StoreManager.sales AS S
+      JOIN StoreManager.sales_products AS P 
+      WHERE S.id = P.sale_id
+      ORDER BY P.sale_id, P.product_id;`,
+  );
+  return response;
+};
+
+const getIdSalesModels = async (id) => {
+  const [response] = await connection.execute(
+    `SELECT S.date, P.product_id AS productId, P.quantity 
+    FROM StoreManager.sales AS S
+    JOIN StoreManager.sales_products AS P 
+    ON P.sale_id = S.id
+    WHERE P.sale_id = (?)
+    ORDER BY P.product_id;
+`, [id],
+  );
+  console.log(response, id);
+  return response;
+};
+
+module.exports = {
+  newSalesDateModels,
+  addSalesDBModels,
+  getAllSalesModels,
+  getIdSalesModels,
+};
