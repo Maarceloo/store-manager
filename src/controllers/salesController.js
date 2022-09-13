966f105a3217f75d458d91253f73e92855173c6f
Marcelo De Lima <delima_marcelo@outlook.com>
2022-09-13 11:43:04 -0300

    feat: salesController implementado

--- src/controllers/salesController.js
+++ src/controllers/salesController.js
@@ -1,7 +1,8 @@
 const salesServices = require('../services/salesServices');
 
 const newSalesController = async (req, res) => { 
-  const result = await salesServices.newSalesServices(req); /* Salva no banco e retorna o novo objeto salvo */
+  const sales = req.body;
+  const result = await salesServices.newSalesServices(sales); /* Salva no banco e retorna o novo objeto salvo */
   res.status(201).json(result);
  };
 
